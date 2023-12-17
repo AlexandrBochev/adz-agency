a15925a5f0a4bcc32a0ccc1abdc730181f70c869
Alexandr Bochev <abochev@gmail.com>
2023-12-17 20:40:26 +0200

    test

--- src/components/Cards/Cards.tsx
+++ src/components/Cards/Cards.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { Card } from "../Card/Card"
 import { Button } from "../Button/Button"
 import { Preloader } from "../Preloader/Preloader"
@@ -10,13 +10,7 @@ const Cards = ({ reload, setReload }: CardsProps) => {
   const [isBtnShow, setIsBtnShow] = useState(true)
   const [isLoading, setIsLoading] = useState(false)
 
-  useEffect(() => {
-    if (reload) {
-      setCount(6)
-      setIsBtnShow(true)
-      setReload(false)
-    }
-  }, [reload])
+  const tergetRef = useRef<HTMLDivElement>(null)
 
   useEffect(() => {
     const getUsers = async () => {
@@ -25,23 +19,33 @@ const Cards = ({ reload, setReload }: CardsProps) => {
         const response = await fetch(`${API_URL}users?page=1&count=${count}`)
         const data = await response.json()
         setUsers(data.users)
-        count > data.users.length && setIsBtnShow(false)
+        count > data.users.length || count === 100 && setIsBtnShow(false)
         setIsLoading(false)
       } catch (error) {
         alert(error)
       }
     }
     getUsers()
-  }, [count])
+  }, [count, users.length])
+
+  useEffect(() => {
+    if (reload) {
+      setUsers([])
+      setCount(6)
+      setIsBtnShow(true)
+      setReload(false)
+    }
+    !reload && tergetRef.current?.focus()
+  }, [reload])
 
   return (
-    <section className='text-center pt-[8.75rem]' id="users">
+    <section className='text-center pt-[8.75rem]' id="users" tabIndex={-1} ref={ tergetRef }>
       <h2 className='mb-[3.125rem]'>Working with GET request</h2>
       <ul className='grid grid-cols-1 gap-5 md:grid-cols-2 md:gap-4 lg:grid-cols-3 lg:gap-[1.875rem] mb-[3.125rem]'>
         { users.map((user: User) => <li key={user.id}><Card user={user} /></li>) }
       </ul>
       { isLoading && <Preloader /> }
-      { isBtnShow && <Button title={ 'Show more' } onClick={ () => setCount(count + 6) } /> }
+      { isBtnShow && <Button title={ 'Show more' } onClick={ () => count + 6 > 100 ? setCount(100) : setCount(count + 6)} /> }
     </section>
   )
 }
